f53b707c9ac3da018160e594bd0e7ab8e854472b
pabbanihanthkumarpab <45432287+pabbanihanthkumarpab@users.noreply.github.com>
2019-10-06 16:13:53 +0530

    Create pipelinejob.groovy

---
+++ pipelinejob.groovy
@@ -0,0 +1,25 @@
+pipelineJob('pipeline-job') {
+  definition {
+    cps {
+      script('''
+        pipeline {
+            agent any
+                stages {
+                    stage('Stage 1') {
+                        steps {
+                            echo 'logic1'
+                        }
+                    }
+                    stage('Stage 2') {
+                        steps {
+                            echo 'logic2'
+                        }
+                    }
+                }
+         
+        }
+      '''.stripIndent())
+      sandbox()     
+    }
+  }
+}
