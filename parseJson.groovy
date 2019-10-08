a22840418e6f9cc4e3bd66999f3ff3528b0a711b
Pabba <Nihanth.Pabba@nielsen.com>
2019-10-08 13:11:46 +0530

    making in a same file

--- parseJson.groovy
+++ parseJson.groovy
@@ -16,3 +16,39 @@
         }
         
     }
+	
+	
+def createPipelineJob( def projectName , def subModuleName , def type){
+
+String basePath = 'CDAR-DEV/$projectName'
+
+folder("$basePath")
+
+
+pipelineJob("$basePath/pipeline-job") {
+  definition {
+    cps {
+      script('''
+        pipeline {
+            agent any
+                stages {
+                    stage('Stage 1') {
+                        steps {
+                            echo 'logic new'
+                        }
+                    }
+                    stage('Stage 2') {
+                        steps {
+                            echo 'logic2'
+                        }
+                    }
+                }
+         
+        }
+      '''.stripIndent())
+      sandbox()     
+    }
+  }
+}
+}
+
