d644d9bc109e7f08c054b52120f7013e6a49c6dd
pabbanihanthkumarpab <45432287+pabbanihanthkumarpab@users.noreply.github.com>
2019-10-09 13:45:45 +0530

    Update parseJson.groovy

--- parseJson.groovy
+++ parseJson.groovy
@@ -1,7 +1,7 @@
     import net.sf.json.groovy.JsonSlurper 
     import hudson.FilePath
     import hudson.*
-    def jobFun = load 'pipelinejob.groovy'
+   
 
    // evaluate(new File("pipelinejob.groovy"))
     //evaluate(new File("./pipelinejob.groovy"))
@@ -19,8 +19,9 @@
     def iterateEachProject (def projectName , def subModules , def type){
   
         def subProjects = subModules.split(',');
-        for  ( int j = 0 ; j<subProjects.size() ; j++){    
-    		  jobFun.createPipelineJob(projectName,subProjects.getAt(j),type);
+        for  ( int j = 0 ; j<subProjects.size() ; j++){  
+		 def jobFun = load 'pipelinejob.groovy'
+    		 jobFun.createPipelineJob(projectName,subProjects.getAt(j),type);
         }
         
     }
