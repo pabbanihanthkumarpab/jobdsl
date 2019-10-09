25b9e2a37b284e1a9f65c7f4936654841a828537
pabbanihanthkumarpab <45432287+pabbanihanthkumarpab@users.noreply.github.com>
2019-10-09 13:04:47 +0530

    Update parseJson.groovy

--- parseJson.groovy
+++ parseJson.groovy
@@ -2,6 +2,7 @@
     import hudson.FilePath
     import hudson.*
 
+    evaluate(new File("pipelinejob.groovy"))
 
     def jsonSlurper = new JsonSlurper();
     println $WORKSPACE
@@ -22,42 +23,5 @@
     }
 	
 	
-def createPipelineJob( def projectName , def subModuleName , def type){
 
-//String basePath = 'CDAR-DEV/$projectName'
-
-//folder("$basePath")
- println "projectName --> " + projectName;
-String projectPath = 'CDAR-DEV/'+projectName;
-String basePath = 'CDAR-DEV/'+projectName+'/'+subModuleName;
-folder("CDAR-DEV")
-folder(projectPath)
-folder(basePath)
-
-pipelineJob("$basePath/pipeline-job") {
-  definition {
-    cps {
-      script('''
-        pipeline {
-            agent any
-                stages {
-                    stage('Stage 1') {
-                        steps {
-                            echo 'logic new 1'
-                        }
-                    }
-                    stage('Stage 2') {
-                        steps {
-                            echo 'logic2'
-                        }
-                    }
-                }
-         
-        }
-      '''.stripIndent())
-      sandbox()     
-    }
-  }
-}
-}
 
