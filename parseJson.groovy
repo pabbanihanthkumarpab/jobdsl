8f71b880f56162bc7bf8c4f79625ce427b2f7a84
pabbanihanthkumarpab <45432287+pabbanihanthkumarpab@users.noreply.github.com>
2019-10-09 13:33:28 +0530

    Update parseJson.groovy

--- parseJson.groovy
+++ parseJson.groovy
@@ -1,6 +1,7 @@
     import net.sf.json.groovy.JsonSlurper 
     import hudson.FilePath
     import hudson.*
+	    import pipelinejob.groovy
 
    // evaluate(new File("pipelinejob.groovy"))
     //evaluate(new File("./pipelinejob.groovy"))
