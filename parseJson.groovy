30fd4f58705b99aac609433baa662b06a9eae0ae
pabbanihanthkumarpab <45432287+pabbanihanthkumarpab@users.noreply.github.com>
2019-10-13 12:49:54 +0530

    Update parseJson.groovy

--- parseJson.groovy
+++ parseJson.groovy
@@ -68,7 +68,7 @@
                       } 
 	         ant {
                      target('Deploy_Service')        
-                     props('env_pass' : '"ssh_${environment}_password"' , 'ssh_service_pwd' : '"${!env_pass}"' , 'ssh_user' : 'docker' , 'repositoryName' : '$repositoryName' , 'subModuleName' : '$subModuleName')
+                     props('env_pass' : '"ssh_${environment}_password"' , 'ssh_service_pwd' : '"${!env_pass}"' , 'ssh_user' : 'docker' , 'repositoryName' : '$repositoryName' , 'subModuleName' : '$subModuleName' , 'env' : '$environment')
                      buildFile('${workspace}/deployment-scripts/${repositoryName}/build/build.xml')
 	         }
 	      }
